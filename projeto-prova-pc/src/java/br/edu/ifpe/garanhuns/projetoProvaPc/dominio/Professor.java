46ff4de71988d713ca081caaa0eccbb013c69f27
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2015-09-15 19:42:35 -0300

    Adicionado Professor e Prova, por Daniel

---
+++ projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/Professor.java
@@ -0,0 +1,45 @@
+package br.edu.ifpe.garanhuns.projetoProvaPc.dominio;
+
+import java.util.*;
+public class Professor {
+
+	private String nome;
+	private int siap; 
+	private String senha;
+
+	Professor(String nome, int siap, String senha){
+		this.nome = nome;
+		this.siap = siap;
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getSiap() {
+		return siap;
+	}
+	public void setSiap(int siap) {
+		this.siap = siap;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void createProva(){
+
+	}
+	public void applyProva(){
+
+	}
+
+
+
+
+}
---
+++ projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/Prova.java
@@ -0,0 +1,10 @@
+package br.edu.ifpe.garanhuns.projetoProvaPc.dominio;
+
+import java.util.*;
+public class Prova {
+
+	private String tema;
+	private ArrayList<Questao> questoes = new ArrayList<Questao>();
+
+
+}
