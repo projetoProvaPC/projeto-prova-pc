7d352e35f443b511de610745443658ced316b7c3
Gabriel Alves <Gabriel Alves@10.28.3.207>
2015-09-15 17:43:52 -0300

    Adicionei equals() a Aluno

--- projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/Aluno.java
+++ projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/Aluno.java
@@ -1,5 +1,7 @@
 package br.edu.ifpe.garanhuns.projetoProvaPc.dominio;
 
+import java.util.Objects;
+
 public class Aluno {
     
     private String nome;
@@ -9,8 +11,30 @@ public class Aluno {
         this.nome = nome;
         this.matricula = matricula;
     }
-    
 
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aluno other = (Aluno) obj;
+        if (!Objects.equals(this.matricula, other.matricula)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+   
     public String getNome() {
         return nome;
     }
