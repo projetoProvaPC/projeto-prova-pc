8fde21f7d9286139d2465f801d49a40d746e0cc4
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2015-09-21 17:45:23 -0300

    Adicionei classe RespostaQuestao.

--- projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/RespostaQuestao.java
+++ projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/RespostaQuestao.java
@@ -1,15 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.edu.ifpe.garanhuns.projetoProvaPc.dominio;
 
 /**
- *
  * @author lucas
+ * @param <T>
  */
-public class RespostaQuestao {
+
+// IMPORTANTE!
+// Esta classe guarda a resposta de um aluno a uma determinada questão
+// É abstract por que pode ser RepostaQuestaoMultiplaEscolha ou
+// RepostaQuestaoVerdadeiroOuFalso
+public abstract class RespostaQuestao<T extends Questao> {
+    
+    private T questao;
+    private Aluno aluno;
+
+    public T getQuestao() {
+        return questao;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    protected void setQuestao(T questao) {
+        this.questao = questao;
+    }
+
+    protected void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+    
+    public abstract double calcularPontuacao();
     
 }
