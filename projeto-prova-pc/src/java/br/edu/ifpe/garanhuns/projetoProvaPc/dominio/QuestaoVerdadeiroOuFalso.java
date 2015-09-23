59e6b4bd3fc4502457d039c20c44bc4099343e21
Lucas Dantas Gueiros <lucasdantasgueiros@gmail.com>
2015-09-22 21:29:39 -0300

    Corrigi as classes de questões Verdadeiro ou Falso?

--- projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/QuestaoVerdadeiroOuFalso.java
+++ projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/QuestaoVerdadeiroOuFalso.java
@@ -3,7 +3,7 @@ package br.edu.ifpe.garanhuns.projetoProvaPc.dominio;
 /** * @author lucas
  */
 public class QuestaoVerdadeiroOuFalso extends Questao {
-
+    
   private boolean [] correta;
 
   public QuestaoVerdadeiroOuFalso (String enunciado, int pontuacao, 
@@ -14,12 +14,20 @@ public class QuestaoVerdadeiroOuFalso extends Questao {
       throw new Exception("Quantidade de afirmacoes errada.");
       
     for(int i = 0 ; i < 5 ; i++) {
-      Afirmacao a = afirmacoes.get(i);
-      correta[i] = afirmacoes.get(i).veracidade();
-      super.add(afirmacoes.get(i));
+      Afirmacao a = afirmacoes[i];
+      correta[i] = a.veracidade();
+      super.add(a);
     }
     
   }
+
+    public double getPercentualCorreto(boolean [] repostas) {
+        double r = 0;
+        for (int i = 0; i < 5; i++) {
+            if (!(correta[i] ^ repostas[i])) r += 0.20;
+        }
+        return r;
+    }
 
   
 
--- projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/RespostaQuestaoVerdadeiroOuFalso.java
+++ projeto-prova-pc/src/java/br/edu/ifpe/garanhuns/projetoProvaPc/dominio/RespostaQuestaoVerdadeiroOuFalso.java
@@ -1,24 +1,20 @@
 package br.edu.ifpe.garanhuns.projetoProvaPc.dominio;
 
 /**
- *
  * @author lucas
  */
-public class RespostaQuestaoMultiplaEscolha extends RespostaQuestao<QuestaoVerdadeiroOuFalso>{
+public class RespostaQuestaoVerdadeiroOuFalso extends RespostaQuestao<QuestaoVerdadeiroOuFalso>{
 
     private boolean [] resposta;
 
-    public RespostaQuestaoMultiplaEscolha(QuestaoVerdadeiroOuFalso questao, boolean [] resposta) throws Exception {
+    public RespostaQuestaoVerdadeiroOuFalso(QuestaoVerdadeiroOuFalso questao, boolean [] resposta) throws Exception {
         super(questao);
-        if(resposta.length > 5)
-            throw new Exception("Reposta inválida");
-        
     }
     
     @Override
     public double calcularPontuacao() {
         // ou tudo ou nada
-        return super.getQuestao().isCorrect(resposta) ? 1 : 0;
+        return super.getQuestao().getPercentualCorreto(resposta) * super.getQuestao().getPontuacao();
     }
 
     @Override
